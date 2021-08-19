af03a24e22b29f4fcec9c901c8e66a3368e83940
unknown <mah_imad@yahoo.fr>
2021-08-19 21:35:47 +0100

    add passport local

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
 // const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const numberRound = 10;
+// const bcrypt = require("bcrypt");
+// const numberRound = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,20 +20,40 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+//app.set('trust proxy', 1);// trust first proxy
+app.use(session({
+  secret: 'any secret ket.',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session())
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+//mongoose.set("useCreateIndex", true);
 
 userSchema = new mongoose.Schema({
-  email: String,
+  username: String,
   password: String
 });
 
 
 //userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
+
+
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/", function(req, res) {
   res.render("home")
 });
@@ -43,48 +66,53 @@ app.get("/register", function(req, res) {
   res.render("register")
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req,res) {
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function(req, res) {
-  bcrypt.hash(req.body.password, numberRound, function(err, hash) {
-    const user = new User({
-      email: req.body.username,
-      // password: md5(req.body.password)
-      password: hash
-    });
-
-
-  user.save(function(err) {
+  console.log(req.body);
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if (err) {
       console.log(err);
+      res.redirect("/register");
     } else {
-      res.render("secrets");
+      passport.authenticate("local")(req, res, function(){
+        console.log(user);
+        res.redirect("/secrets")
+
+      });
     }
-  });
   });
 });
 
 app.post("/login", function(req, res) {
-      const username = req.body.username;
-      //const password = md5(req.body.password);
 
-      User.findOne({
-        email: username
-      }, function(err, foundUser) {
-        if (err) {
-          console.log(err);
-        } else {
-          if (foundUser) {
-            // if (foundUser.password === password) {
-            //   res.render("secrets");
-            // };
-            bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-              if (result) {
-                res.render("secrets");
-              }
-            });
-          };
-        };
-      });
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local", { failureFlash: 'Invalid username or password.' })(req,res, function(){
+        res.redirect("/secrets")
+      });
+    }
+  });
+
+});
 
       app.listen(3000, function() {
         console.log("server is running on 3000");
